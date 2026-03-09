3fdb97b22ddab3055d2195528c0951f1a5a32a38
yslan <r05246001@ntu.edu.tw>
2026-03-09 14:37:16 -0500

    orintation is messed up, workaround

--- gen_box1d.m
+++ gen_box1d.m
@@ -2,10 +2,11 @@ function X = gen_box1d(nbx, dist)
 
 switch dist
 case 0
-   X = linspace(-1.0, 1.0, nbx+1);
+   X = linspace(1.0, -1.0, nbx+1);
 case 1 % second kind, includes end points
    X = cos(pi * (0:nbx) / nbx);
 case 2
    [X, ~] = zwgll(nbx);
+   X = X(end:-1:1);
 end
 
